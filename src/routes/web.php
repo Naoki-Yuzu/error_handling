2808f9c022799f6d6b8eec130856fbe7ae555d04
nh <nh@nhnoMacBook-Air.local>
2025-04-11 12:42:06 +0900

    add comment

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Laravelで用意されている400エラーの例外クラスのレスポンスが返却されるEP
 Route::get('/bad-request-exception', [ErrorController::class, 'badRequestException']);
+// 自作の404エラーの例外クラスのレスポンスが返却されるEP
 Route::get('/not-found-exception', [ErrorController::class, 'notFoundException']);
+// PHPで用意されているベース例外クラスのレスポンスが返却されるEP
 Route::get('/unprocessable-entity-exception', [ErrorController::class, 'unprocessableEntityException']);
